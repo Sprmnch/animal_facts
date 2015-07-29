86f33f33110fb11e924351221d0523fb0e4a6820
Christian de la Cruz <cdelacruzmunoz@palmertrinity.org>
2015-07-29 19:20:22 +0000

    Facts work

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,15 @@
 require 'bundler'
+require_relative 'models/model.rb'
 Bundler.require
 
 class MyApp < Sinatra::Base
 
   get '/' do
+    @fact = get_fact
     erb :index
   end
-
+  get '/POST' do
+    @fact = get_fact
+    erb :results
+  end
 end
