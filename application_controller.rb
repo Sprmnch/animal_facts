f54d8ac14bdd978a43b89b9623e97b7fb5492597
Christian de la Cruz <cdelacruzmunoz@palmertrinity.org>
2015-07-30 14:23:25 +0000

    added gifs

--- application_controller.rb
+++ application_controller.rb
@@ -5,11 +5,11 @@ Bundler.require
 class MyApp < Sinatra::Base
 
   get '/' do
-    @fact = get_fact
+#     @fact = get_fact
     erb :index
   end
   get '/POST' do
-    @fact = get_fact
+    @fact = get_fact()
     erb :results
   end
 end
