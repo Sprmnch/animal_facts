53c62218d3f7bf239855f76db652c81d4b7fe8ec
c650 <cbailey@palmertrinity.org>
2015-07-29 20:28:24 -0400

    saw errors LOL, fixed em

--- application_controller.rb
+++ application_controller.rb
@@ -1,11 +1,10 @@
 require 'bundler'
-require_relative 'models/model.rb'
+require_relative "models/model.rb"
 Bundler.require
 
 class MyApp < Sinatra::Base
 
   get '/' do
-    @fact = get_fact
     erb :index
   end
   get '/get_fact' do
